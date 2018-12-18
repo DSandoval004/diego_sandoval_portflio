d57554b49dfbfdfbb211cbd3b70c5de5b15ac495
Diego Sandoval <dsandoval004@west-mec.org>
2018-12-18 15:28:00 -0700

    Responsive
    Added responsive design, fixed a few more styles and started with show and hide JavaScript

---
+++ JavaScript/index.js
@@ -0,0 +1,17 @@
+// Nav Icon
+// About
+function aboutShow() {
+    document.getElementById('contAbout').style.display = 'block'
+    document.getElementById('sectionHide').style.display = 'block'
+    document.getElementById('sectionShow').style.display = 'none'
+}
+
+function aboutHide() {
+    document.getElementById('contAbout').style.display = 'none'
+    document.getElementById('sectionHide').style.display = 'none'
+    document.getElementById('sectionShow').style.display = 'block'
+}
+// Education
+// Skills
+// Resume
+// Contact
