1959b9023f77eb09af9d777d61b70922bf9c1a92
Diego Sandoval <dsandoval004@west-mec.org>
2018-12-18 22:58:55 -0700

    + and - buttons to show and display sections
    Added JavaScript and HTML to make all the sections have a plus button that shows the content, then it turns into a minus button to hide the content.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,17 +1,61 @@
 // Nav Icon
 // About
-function aboutShow() {
+function AboutShow() {
     document.getElementById('contAbout').style.display = 'block'
-    document.getElementById('sectionHide').style.display = 'block'
-    document.getElementById('sectionShow').style.display = 'none'
+    document.getElementById('sectionHideAbout').style.display = 'block'
+    document.getElementById('sectionShowAbout').style.display = 'none'
 }
 
-function aboutHide() {
+function AboutHide() {
     document.getElementById('contAbout').style.display = 'none'
-    document.getElementById('sectionHide').style.display = 'none'
-    document.getElementById('sectionShow').style.display = 'block'
+    document.getElementById('sectionHideAbout').style.display = 'none'
+    document.getElementById('sectionShowAbout').style.display = 'block'
 }
 // Education
+function EDShow() {
+    document.getElementById('contED').style.display = 'block'
+    document.getElementById('sectionHideED').style.display = 'block'
+    document.getElementById('sectionShowED').style.display = 'none'
+}
+
+function EDHide() {
+    document.getElementById('contED').style.display = 'none'
+    document.getElementById('sectionHideED').style.display = 'none'
+    document.getElementById('sectionShowED').style.display = 'block'
+}
 // Skills
+function SkillsShow() {
+    document.getElementById('contSkills').style.display = 'block'
+    document.getElementById('sectionHideSkills').style.display = 'block'
+    document.getElementById('sectionShowSkills').style.display = 'none'
+}
+
+function SkillsHide() {
+    document.getElementById('contSkills').style.display = 'none'
+    document.getElementById('sectionHideSkills').style.display = 'none'
+    document.getElementById('sectionShowSkills').style.display = 'block'
+}
 // Resume
+function ResumeShow() {
+    document.getElementById('contResume').style.display = 'block'
+    document.getElementById('sectionHideResume').style.display = 'block'
+    document.getElementById('sectionShowResume').style.display = 'none'
+}
+
+function ResumeHide() {
+    document.getElementById('contResume').style.display = 'none'
+    document.getElementById('sectionHideResume').style.display = 'none'
+    document.getElementById('sectionShowResume').style.display = 'block'
+}
 // Contact
+function ContactShow() {
+    document.getElementById('contContact').style.display = 'block'
+    document.getElementById('sectionHideContact').style.display = 'block'
+    document.getElementById('sectionShowContact').style.display = 'none'
+}
+
+function ContactHide() {
+    document.getElementById('contContact').style.display = 'none'
+    document.getElementById('sectionHideContact').style.display = 'none'
+    document.getElementById('sectionShowContact').style.display = 'block'
+}
